d8389c49b91ce8b0042c17010843f3b72c9574ac
robert8p <robert8p@gmail.com>
2026-08-20 12:08:39 +0100

    Normalize SEC payloads before scanner persistence

--- app/__init__.py
+++ app/__init__.py
@@ -12,14 +12,17 @@ import sys
 
 from . import oversold_scoring_v32 as _oversold_scoring
 from . import oversold_scoring_v33 as _oversold_v33_impl
+from . import oversold_sec_fundamentals as _oversold_sec
 from . import oversold_tracking as _oversold_tracking
 from .oversold_scoring_v32_compat import patch_module as _patch_v32
 from .oversold_scoring_v33 import patch_module as _patch_v33
 from .oversold_scoring_v33_compat import patch_module as _patch_v33_compat
 from .oversold_scoring_v33_runtime import patch_module as _patch_v33_runtime
+from .oversold_sec_json_compat import patch_module as _patch_sec_json
 from .oversold_three_session_target import patch_scoring as _patch_three_session_target
 from .oversold_tracking_day3 import patch_module as _patch_tracking_day3
 
+_patch_sec_json(_oversold_sec)
 _patch_v32(_oversold_scoring)
 sys.modules[f"{__name__}.oversold_scoring"] = _oversold_scoring
 _patch_three_session_target(_oversold_scoring)
